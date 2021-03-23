8ea0e1893c56cf19790f6d012b3c962c9da9ba0b
Riccardo Musso <rik.musso@gmail.com>
2021-03-23 15:26:47 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@ sudo apt-get update
 sudo apt-get install -y vim-nox curl git python python3 python3-dev lua5.2 liblua5.2-dev ruby2.7 clang-format
 sudo apt-get install -y build-essential cmake libclang-dev libncurses-dev libz-dev xz-utils libpthread-workqueue-dev
 
-# Vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp .vimrc ~/.vimrc
+ln -s "$PWD/.vimrc" ~/.vimrc
+ln -s "$PWD/.vim" ~/.vim
+vim -c "PlugUpdate" -c "PlugInstall" -c "qa!"
 
-vim -c "PlugUpdate" -c "PlugInstall" -c "q!"
 cd ~/.vim/plugged/YouCompleteMe
+git submodule update --init --recursive
 sudo python3 install.py --clang-completer
